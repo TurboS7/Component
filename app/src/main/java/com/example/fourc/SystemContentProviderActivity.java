ae090aeafb72a3c30ebc5557cea0c7fd3b3a9e6d
TurboS7 <872318839@qq.com>
2021-09-17 18:41:49 +0800

    完善了对系统通讯录的增删改查的功能

--- app/src/main/java/com/example/fourc/SystemContentProviderActivity.java
+++ app/src/main/java/com/example/fourc/SystemContentProviderActivity.java
@@ -23,8 +23,7 @@ import android.widget.Toast;
 import java.net.URI;
 
 public class SystemContentProviderActivity extends AppCompatActivity {
-    private  ContentResolver resolver;
-    private EditText et_uname,et_phone;
+    private EditText et_uname,et_phone,et_number;
     private TextView tv_info;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,18 +32,19 @@ public class SystemContentProviderActivity extends AppCompatActivity {
         /*申请权限*/
         requestPermission();
         initUI();/*初始化UI*/
-        initData();/*初始化数据*/
+//        initData();/*初始化数据*/
 
     }
     /*初始化UI*/
     private void initUI(){
-    et_phone = findViewById(R.id.et_upsw);
+    et_phone = findViewById(R.id.et_phone);
     et_uname = findViewById(R.id.et_uname);
+    et_number = findViewById(R.id.et_number);
     tv_info = findViewById(R.id.tv_info);
     }
-    private void initData(){
-        resolver = getContentResolver();/*获取ContentResolver*/
-    }
+//    private void initData(){
+//        resolver = getContentResolver();/*获取ContentResolver*/
+//    }
     /*获取权限*/
     private void requestPermission(){
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -56,6 +56,7 @@ public class SystemContentProviderActivity extends AppCompatActivity {
     }
     /*插入数据*/
     public void onInsert(View view) {
+        ContentResolver resolver = getContentResolver();
         String name = et_uname.getText().toString();
         String phone = et_phone.getText().toString();
         /*创建一个空的ContentValues*/
@@ -77,37 +78,54 @@ public class SystemContentProviderActivity extends AppCompatActivity {
         /*向联系人URI添加联系人姓名*/
         getContentResolver().insert(ContactsContract.Data.CONTENT_URI,values);
 
-        /*1.清空values*/
+        /**
+         * 1.清空values
+         * 2.设置id
+         * 3.设置内容类型
+         * 4.设置联系人电话
+         * 5.设置电话类型
+         */
         values.clear();
-        /*2.设置id*/
         values.put(ContactsContract.Data.RAW_CONTACT_ID, rawContactId);
-        /*3.设置内容类型*/
         values.put(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE);
-        /*4.设置联系人电话*/
         values.put(ContactsContract.CommonDataKinds.Phone.NUMBER,phone);
-        /*5.设置电话类型*/
         values.put(ContactsContract.CommonDataKinds.Phone.TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE);
-        /*插入通话*/
         resolver.insert(ContactsContract.Data.CONTENT_URI , values);
         showToast("联系人："+name+"-"+phone+"成功存入通讯录中！");
     }
     /*删除数据*/
     public void onDelete(View view) {
-        
+        ContentResolver resolver = getContentResolver();
+        int number = Integer.valueOf(et_number.getText().toString());
+        int result = resolver.delete(ContactsContract.RawContacts.CONTENT_URI, ContactsContract.RawContacts._ID + "=?",
+                new String[]{String.valueOf(number)});
+        System.out.println("number = "+ number);
+        System.out.println(result);
     }
     /*更新数据*/
     public void onUpdate(View view) {
-
+        ContentResolver resolver = getContentResolver();
+        ContentValues values = new ContentValues();
+        values.put(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME, et_uname.getText().toString());
+        values.put(ContactsContract.CommonDataKinds.Phone.NUMBER,et_phone.getText().toString());
+        int result = resolver.update(ContactsContract.Data.CONTENT_URI, values,
+                ContactsContract.Data.RAW_CONTACT_ID +"=?",new String[]{
+                        String.valueOf(et_number.getText().toString())});
     }
     /*查询数据*/
     public void onQuery(View view) {
         /*获取Resolver*/
-        String text = "";
+        ContentResolver resolver = getContentResolver();
+        String text = null;
+        tv_info.setText("");
         Cursor cursor = resolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,null,null,null);
         while (cursor.moveToNext()){
             String name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
             String number = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-            text  = text+"用户名："+name + "\n" + "电话号码："+number +"\n";
+            String ID = cursor.getString(cursor.getColumnIndex(ContactsContract.RawContacts._ID));
+            int num = Integer.valueOf(ID)/2;
+            System.out.println("ID"+ID);
+            text  = text+"用户名："+name + "电话号码："+number+"[ID:"+num+"]" +"\n" ;
             tv_info.setText(text);
         }
         cursor.close();
