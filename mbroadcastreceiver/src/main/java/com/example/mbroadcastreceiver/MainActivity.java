980daf00d58b80dddc5e60f8edb4364f851f7e11
TurboS7 <872318839@qq.com>
2021-09-24 21:59:04 +0800

    添加了简单的广播接受者

--- mbroadcastreceiver/src/main/java/com/example/mbroadcastreceiver/MainActivity.java
+++ mbroadcastreceiver/src/main/java/com/example/mbroadcastreceiver/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.mbroadcastreceiver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    private MBroadCastReceiver receiver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        receiver = new MBroadCastReceiver();
+        IntentFilter filter = new IntentFilter("test");
+        filter.setPriority(10);
+        registerReceiver(receiver,filter);
+    }
+
+//    private void init() {
+//
+//
+//    }
+
+    public void click(View view) {
+        Intent intent = new Intent();
+        intent.setAction("test");
+        intent.putExtra("data","2321321321");
+        sendBroadcast(intent);
     }
 }
